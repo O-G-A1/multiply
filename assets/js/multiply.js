a5c6afb0e192415afb2857374134f811223fa09e
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-07-28 09:46:52 +0100

    multiplyjs

---
+++ assets/js/multiply.js
@@ -0,0 +1,52 @@
+let firstNumber = parseInt(prompt("Enter your first number"));
+let secondNumber = parseInt(prompt("Enter your second number"));
+
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*1);
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*2);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*3);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*4);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*5);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*6);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*7);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*8);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*9);
+    
+}
+console.log("Multiplication table for chosen numbers as it increases by 1");
+for(g=firstNumber; g<=secondNumber; g++){
+    console.log(g*10);
+    
+}
